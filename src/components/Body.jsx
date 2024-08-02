531d6ae9b0975ef0bb5140a9ec6dc729dc27e11b
Stalin S <60595536+stalinTechXD@users.noreply.github.com>
2024-08-02 16:04:07 +0530

    added search container

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,22 @@
 import {restrauntList} from './../config'
 import {RestrauntCard} from './RestrurantCard'
+import React from 'react';
 const Body = () =>
     {
               const start = 3; // 4th iteration (0-based index)
               const end = 10; // 7th iteration (0-based index)
               return (
+                <>
+                    <div className="search-container">
+                        <input type="text" className="search-input" placeholder="Search" value=""/> 
+                        <button className="search">Search</button>
+                    </div>
                 <div className="restaurant-list">
                     {restrauntList.slice(start, end).map((res) => {
                         return <RestrauntCard {...res.card.card} key={res.card.card.info.id} />;
                     })}
                 </div>
+                </>
             );
     }
 
